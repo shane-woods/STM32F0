83e1b1bbbb4f6a921252e963670558f439344565
Shane <shwoods35@gmail.com>
2022-06-30 11:05:19 -0400

    altered makefile

--- spi_eadc_code/spi_eadc.c
+++ spi_eadc_code/spi_eadc.c
@@ -21,6 +21,7 @@
 #endif
 
 #include "spi_eadc.h"
+#include "../uart.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -28,7 +29,6 @@
 
 static void clock_setup(void)
 {
-
     /* Enable clock at 48mhz */
     rcc_clock_setup_in_hsi_out_48mhz();
 
@@ -43,9 +43,35 @@ static void clock_setup(void)
     rcc_periph_clock_enable(DAC_CLOCK);
 }
 
+static void gpio_setup(void)
+{
+    /** LED GPIO SETUP **/
+    /* Set GPIO8 (in GPIO port C) to 'output push-pull'. */
+    gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, BLUE_LED_PIN | GREEN_LED_PIN);
+
+    /** SPI GPIO SETUP **/
+    /* Configure CNV GPIO as PA8 (TIM1_CH1) */
+    gpio_mode_setup(CNV_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, CNV_GPIO_PIN);
+    gpio_set_af(CNV_GPIO_PORT, GPIO_AF2, CNV_GPIO_PIN);
+    gpio_set_output_options(CNV_GPIO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, CNV_GPIO_PIN);
+
+    /* Configure SCK GPIO as PA5 */
+    gpio_mode_setup(SCK_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SCK_GPIO_PIN);
+    gpio_set_af(SCK_GPIO_PORT, GPIO_AF0, SCK_GPIO_PIN);
+    gpio_set_output_options(SCK_GPIO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, SCK_GPIO_PIN);
+
+    /* Configure MISO GPIO as PA6 */
+    gpio_mode_setup(MISO_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, MISO_GPIO_PIN);
+    gpio_set_af(MISO_GPIO_PORT, GPIO_AF0, MISO_GPIO_PIN);
+    gpio_set_output_options(MISO_GPIO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, MISO_GPIO_PIN);
+
+    /* Setup GPIO pin GPIO_USART1_TX/GPIO9 on GPIO port A for transmit. */
+    gpio_mode_setup(USART_TX_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, USART_TX_GPIO_PIN);
+    gpio_set_af(USART_TX_GPIO_PORT, GPIO_AF1, USART_TX_GPIO_PIN);
+}
+
 static void spi_setup(void)
 {
-
     /* Reset SPI, SPI_CR1 register cleared, SPI is disabled */
     spi_reset(SPI1);
 
@@ -78,12 +104,11 @@ static void spi_setup(void)
 
 static void timer_setup(void)
 {
-
     /* Disable/Reset Timer? */
-    timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP); // FIXME THIS IS NEW
+    timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP); // FIXME THIS IS NEW
 
     /* Enables TIM1_CH1 in Output Compare Mode */
-    timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_FORCE_HIGH);
+    timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_TOGGLE);
     timer_enable_oc_output(TIM1, TIM_OC1);
     timer_enable_break_main_output(TIM1);
 
@@ -108,59 +133,11 @@ static void timer_setup(void)
     TIM_CR1(TIM1) |= TIM_CR1_CEN;
 }
 
-static void usart_setup(void)
-{
-    /* Setup GPIO pin GPIO_USART1_TX/GPIO9 on GPIO port A for transmit. */
-    gpio_mode_setup(USART_TX_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, USART_TX_GPIO_PIN);
-    gpio_set_af(USART_TX_GPIO_PORT, GPIO_AF1, USART_TX_GPIO_PIN);
-
-    /* Setup UART parameters. */
-
-    /* Baudrate at 115200 bits/sec */
-    usart_set_baudrate(USART1, 115200);
-
-    usart_set_databits(USART1, 8);
-    usart_set_stopbits(USART1, USART_CR2_STOPBITS_1);
-    usart_set_mode(USART1, USART_MODE_TX);
-    usart_set_parity(USART1, USART_PARITY_NONE);
-    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-
-    /* Finally enable the USART. */
-    usart_enable(USART1);
-}
-
-static void gpio_setup(void)
-{
-    /** LED GPIO SETUP **/
-    /* Set GPIO8 (in GPIO port C) to 'output push-pull'. */
-    gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, BLUE_LED_PIN | GREEN_LED_PIN);
-
-    /** SPI GPIO SETUP **/
-    /* Configure CNV GPIO as PA8 (TIM1_CH1) */
-    gpio_mode_setup(CNV_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, CNV_GPIO_PIN);
-    gpio_set_af(CNV_GPIO_PORT, GPIO_AF2, CNV_GPIO_PIN);
-    gpio_set_output_options(CNV_GPIO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, CNV_GPIO_PIN);
-
-    /* Configure SCK GPIO as PA5 */
-    gpio_mode_setup(SCK_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SCK_GPIO_PIN);
-    gpio_set_af(SCK_GPIO_PORT, GPIO_AF0, SCK_GPIO_PIN);
-    gpio_set_output_options(SCK_GPIO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, SCK_GPIO_PIN);
-
-    /* Configure MISO GPIO as PA6 */
-    gpio_mode_setup(MISO_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, MISO_GPIO_PIN);
-    gpio_set_af(MISO_GPIO_PORT, GPIO_AF0, MISO_GPIO_PIN);
-    gpio_set_output_options(MISO_GPIO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, MISO_GPIO_PIN);
-}
-
 void tim1_cc_isr(void)
 {
 
-    int raw; /* 32 bit value */
-    char raw_buf[50];
-    int raw_buf_len;
-
     /* Toggle Blue LED on interrupt */
-    gpio_toggle(LED_PORT, BLUE_LED_PIN);
+    gpio_toggle(LED_PORT, GREEN_LED_PIN);
 
     /* Send byte to initialize SPI transfer */
     SPI1_DR = 0x1;
@@ -168,29 +145,17 @@ void tim1_cc_isr(void)
         ; /* wait for SPI transfer complete */
 
     /* Get raw adc value from data register */
-    raw = SPI1_DR;
+    int raw = SPI1_DR;
 
-    uint8_t MSB = ((raw & 0xFF00) >> 8); /* MSB HERE */
-    uint8_t LSB = (raw & 0xFF);          /* LSB HERE */
-    raw = 0x0;
-    raw = (raw | LSB);
-    raw = raw << 8;
-    raw = raw | MSB;
-
-    raw_buf_len = snprintf(raw_buf, sizeof(raw_buf), "Raw %d", raw);
-    for (int i = 0; i < raw_buf_len; i++)
-        usart_send_blocking(USART1, raw_buf[i]);
-    usart_send_blocking(USART1, '\r');
-    usart_send_blocking(USART1, '\n');
+    /* Send RPA sample (SPI readout has bytes swapped) */
+    putswap(raw);
 
     /**
      * Equation to calculate voltage
      * Voltage = Raw * (5 / 65535)
-     *
      */
-
     /* extern ADC readout completed. Force CNV low */
-    // TIM1_CCMR1 = TIM_CCMR1_OC1M_FORCE_LOW; // (assumes all other bits are zero)
+    TIM1_CCMR1 = TIM_CCMR1_OC1M_FORCE_HIGH; // (assumes all other bits are zero)
     TIM1_CCMR1 = TIM_CCMR1_OC1M_TOGGLE;
 
     TIM1_SR = ~TIM_SR_CC1IF; // clear interrupt
@@ -199,11 +164,9 @@ void tim1_cc_isr(void)
 int main(void)
 {
     clock_setup();
-    spi_setup();
     gpio_setup();
-    usart_setup();
+    spi_setup();
     timer_setup();
-
     while (1)
         ;
 }
