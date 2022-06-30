69401761380282c3557beca6d37ea918ff3797bf
Jared King <94400363+jck1044@users.noreply.github.com>
2022-06-30 10:35:42 -0400

    Added put methods to .h

--- spi_eadc_code/spi_eadc.h
+++ spi_eadc_code/spi_eadc.h
@@ -46,4 +46,6 @@ static void timer_setup(void);
 static void spi_setup(void);
 static void clock_setup(void);
 static void hk_setup(void);
-
+void putch(void);
+void putwd(void);
+void putswab(void);
